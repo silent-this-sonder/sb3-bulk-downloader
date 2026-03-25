b1b2630c71d34155970a37583ec82ee72817ee3f
silent-this-sonder <143755460+silent-this-sonder@users.noreply.github.com>
2026-03-25 15:11:57 -0400

    Reorder functions, add missing docstrings, and general organization

--- main.py
+++ main.py
@@ -4,10 +4,65 @@ import requests
 import scratchattach as s3
 import shutil
 import string
-import zipfile
 import time as t
+import zipfile
 
 # BASIC FUNCTIONS
+
+def input_scratch_login():
+    '''Log in to Scratch with user and pw; returns the Session object'''
+    while True: 
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+
+        session = None
+        if password:
+            try:
+                session = s3.login(username, password)
+                print("Login successful!")
+                return session
+            except Exception as e:
+                print("Login failed. Try again. Try not to mess up many times or Scratch might flag you as a clanker.")
+            else:
+                return
+
+def menu(arr):
+    '''Displays possibly non-String items in a list, asks users to select one, and returns the index of the actual object in the list'''
+    for i in range(len(arr)):
+        print("\t" + str(i) + ". " + str(arr[i]))
+        
+    choice = -1
+    while not(0 <= int(choice) <= len(arr)-1):
+        choice = input("\tEnter the index of your choice: ")
+        if not choice.isdigit():
+            print("\tInput invalid: index must be a number.")
+            choice = -1
+            continue
+        if not(0 <= int(choice) <= len(arr)-1):
+            print("\tInput invalid: index must be in range.")
+            
+    choice = int(choice)
+    return choice
+
+def process_project_json(project_json_path, asset_dir):
+    '''Download the assets from the project.json'''
+    # asset_dir is the temporary folder for the project to store assets before zipping it up 
+    with open(project_json_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    md5exts = extract_md5exts(data)
+
+    if not md5exts:
+        print("\tNo md5ext assets found in project.json")
+        return
+
+    for md5ext in sorted(md5exts):
+        try:
+            downloaded = download_md5ext(md5ext, asset_dir)
+            print(f"\tDownloaded asset {md5ext} -> {downloaded}")
+        except Exception as e:
+            print(f"\tFailed to download {md5ext}: {e}")
+
 def extract_md5exts(node, seen=None):
     '''Recursive function that finds and returns all the md5exts from the project.json'''
     if seen is None:
@@ -46,93 +101,9 @@ def download_md5ext(md5ext, out_dir):
 
     return local_path
 
-def input_scratch_login():
-    '''Log in to Scratch with user and pw; returns the Session object'''
-    while True: 
-        username = input("Enter username: ")
-        password = input("Enter password: ")
-
-        session = None
-        if password:
-            try:
-                session = s3.login(username, password)
-                print("Login successful!")
-                return session
-            except Exception as e:
-                print("Login failed. Try again. Try not to mess up many times or Scratch might flag you as a clanker.")
-            else:
-                return
-
-def process_project_json(project_json_path, asset_dir):
-    '''Download the assets from the project.json'''
-    # asset_dir is the temporary folder for the project to store assets before zipping it up 
-    with open(project_json_path, "r", encoding="utf-8") as f:
-        data = json.load(f)
-
-    md5exts = extract_md5exts(data)
-
-    if not md5exts:
-        print("\tNo md5ext assets found in project.json")
-        return
-
-    for md5ext in sorted(md5exts):
-        try:
-            downloaded = download_md5ext(md5ext, asset_dir)
-            print(f"\tDownloaded asset {md5ext} -> {downloaded}")
-        except Exception as e:
-            print(f"\tFailed to download {md5ext}: {e}")
-
-def menu(arr):
-    '''Displays possibly non-String items in a list, asks users to select one, and returns the index of the actual object in the list'''
-    for i in range(len(arr)):
-        print("\t" + str(i) + ". " + str(arr[i]))
-        
-    choice = -1
-    while not(0 <= int(choice) <= len(arr)-1):
-        choice = input("\tEnter the index of your choice: ")
-        if not choice.isdigit():
-            print("\tInput invalid: index must be a number.")
-            choice = -1
-            continue
-        if not(0 <= int(choice) <= len(arr)-1):
-            print("\tInput invalid: index must be in range.")
-            
-    choice = int(choice)
-    return choice
-
 # DOWNLOADER FUNCTIONS
-def make_filenames(p, project, translation_table):
-    fn = project.title.translate(translation_table)
-    jsonfile = fn + ".json"  # just name it .json because that's what it is, it's not even a real sb3
-
-    # Removes forbidden characters from file name
-    fnc = "".join(c for c in fn if c.isalnum() or c in (' ', '.', '_')).rstrip()
-
-    project_id = p.id
-    fnc = f"{fnc}_{project_id}" # avoid overwriting projects with identical names
-
-    # Prevent the file name using reserved names
-    reserved_names = {
-        "CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4",
-        "COM5", "COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2",
-        "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"
-    }
-    if fnc.upper() in reserved_names:
-        fnc = f"_{fnc}"
-
-    # In case of empty title
-    if not fnc:
-        fnc = "unnamed project"
-
-    return jsonfile, fnc
-
-def make_sb3_folder(fnc):
-    # Make folder
-    project_dir = os.path.join("downloads", fnc)
-    os.makedirs(project_dir, exist_ok=True)
-    return project_dir
-
 def download_sb3(project, fnc, jsonfile):
+    '''Download the project.json and assets from Scratch into a new directory'''
     project_dir = make_sb3_folder(fnc)
     try:
         project.download(
@@ -164,8 +135,40 @@ def download_sb3(project, fnc, jsonfile):
     )
     return project_dir
 
+def make_sb3_folder(fnc):
+    '''Make the directory for the new sb3'''
+    project_dir = os.path.join("downloads", fnc)
+    os.makedirs(project_dir, exist_ok=True)
+    return project_dir
+
+def make_filenames(p, project, translation_table):
+    '''Remove forbidden characters and return the names for the sb3 files'''
+    fn = project.title.translate(translation_table)
+    jsonfile = fn + ".json"  # just name it .json because that's what it is, it's not even a real sb3
+
+    # Removes forbidden characters from file name
+    fnc = "".join(c for c in fn if c.isalnum() or c in (' ', '.', '_')).rstrip()
+
+    project_id = p.id
+    fnc = f"{fnc}_{project_id}" # avoid overwriting projects with identical names
+
+    # Prevent the file name using reserved names
+    reserved_names = {
+        "CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4",
+        "COM5", "COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2",
+        "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"
+    }
+    if fnc.upper() in reserved_names:
+        fnc = f"_{fnc}"
+
+    # In case of empty title
+    if not fnc:
+        fnc = "unnamed project"
+
+    return jsonfile, fnc
+
 def zip_sb3(fnc, project_dir):
-    # Zip up the completed folder with all the assets
+    '''Zip up the completed folder with all the assets'''
     sb3_filename = f"{fnc}.sb3"
     sb3_path = os.path.join("downloads", sb3_filename)
     with zipfile.ZipFile(sb3_path, 'w', zipfile.ZIP_DEFLATED) as zf:
@@ -204,7 +207,6 @@ if __name__ == "__main__":
         print(p.title)
         project = p
         print("Downloading...")
-        
 
         # Get session id and use to load project
         project = session.connect_project(p.id)
@@ -219,6 +221,8 @@ if __name__ == "__main__":
         
         sb3_path = zip_sb3(fnc, project_dir)
         print(f"Project saved as {sb3_path}")
-        t.sleep(3) # sleep 3 seconds so scratch doesn't rate limit
 
-    print("End of project list.")
+        # sleep 3 seconds so scratch doesn't rate limit
+        t.sleep(3)
+
+    print("\nEnd of project list. There are no more projects to download.")
