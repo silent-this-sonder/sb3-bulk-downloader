d43da6c7fc5fcd887f5d18f3e90462f9504e80c5
silent-this-sonder <143755460+silent-this-sonder@users.noreply.github.com>
2026-03-26 16:32:31 -0400

    Add rudimentary screen switching system

--- gui.py
+++ gui.py
@@ -29,44 +29,76 @@ root = tk.Tk()
 root.title("SB3 Bulk Downloader")
 root.geometry("960x720")
 
-# WIDGETS
+# Rudimentary screen switching
+def switch_to_project_select():
+    login_screen.pack_forget()
+    project_select_screen.pack()
 
-# Login Screen
-user_label = tk.Label(root, text="Username:")
-user_entry = tk.Entry(root)
-pw_label = tk.Label(root, text="Password:")
-pw_entry = tk.Entry(root, show="*")
-login_button = tk.Button(root, text="Login")
+def switch_to_download():
+    project_select_screen.pack_forget()
+    download_screen.pack()
 
-# Project Filtering
+# LOGIN SCREEN
+login_screen = tk.Frame()
+user_label = tk.Label(login_screen, text="Username:")
+user_entry = tk.Entry(login_screen)
+pw_label = tk.Label(login_screen, text="Password:")
+pw_entry = tk.Entry(login_screen, show="*")
+login_button = tk.Button(
+    login_screen, text="Login",
+    command=switch_to_project_select
+    )
+
+user_label.pack()
+user_entry.pack()
+pw_label.pack()
+pw_entry.pack()
+login_button.pack()
+
+# PROJECT SELECT
+project_select_screen = tk.Frame()
 project_opts = ["all", "shared", "unshared"]
-project_label = tk.Label(root, text="Projects to Download")
-project_optmenu = tk.OptionMenu(root, tk.StringVar(value="all"), *project_opts)
-project_selectall_button = tk.Button(root, text="Select all")
-project_checklist = ScrollableChecklist(root, ["example"] * 50)
+project_label = tk.Label(project_select_screen, text="Projects to Download")
+project_optmenu = tk.OptionMenu(project_select_screen, tk.StringVar(value="all"), *project_opts)
+project_selectall_button = tk.Button(project_select_screen, text="Select all")
+project_checklist = ScrollableChecklist(project_select_screen, ["example"] * 50)
+download_button = tk.Button(
+    project_select_screen, text="Download Selected",
+    command=switch_to_download
+)
 
-# Downloading
-download_button = tk.Button(root, text="Download Selected")
+project_label.pack()
+project_optmenu.pack()
+project_selectall_button.pack()
+project_checklist.pack(fill="y", expand=True)
+download_button.pack()
+
+# DOWNLOADING SCREEN
+download_screen = tk.Frame()
 # progress bar for current project
 cur_download_progress = ttk.Progressbar(
-    root, orient="horizontal", length=500, mode="determinate"
+    download_screen, orient="horizontal", length=500, mode="determinate"
 )
 # progress bar for all projects
 all_download_progress = ttk.Progressbar(
-    root, orient="horizontal", length=500, mode="determinate"
+    download_screen, orient="horizontal", length=500, mode="determinate"
 )
 # labels for progress
 cur_download_label = tk.Label(
-    root,
+    download_screen,
     text="Currently downloading [asset title], [num] / [total] assets downloaded"
 )
 all_download_label = tk.Label(
-    root,
+    download_screen,
     text="Currently downloading [project title], [num] / [total] projects downloaded"
 )
 
-# FUNCTIONALITY
+cur_download_progress.pack()
+cur_download_label.pack()
+all_download_progress.pack()
+all_download_label.pack()
 
+# FUNCTIONALITY
 # TODO: I want to organize the tkinter gui file a bit and make it only handle the widget display
     # The code should be modular to keep it very neat
     # I don't have the structure fully fleshed out yet but according to TkinterBuilder,
@@ -79,26 +111,7 @@ all_download_label = tk.Label(
 # TODO: code for progress labels to update based on info
 
 # GEOMETRY MANAGER
-
-# Login Screen
-user_label.pack()
-user_entry.pack()
-pw_label.pack()
-pw_entry.pack()
-login_button.pack()
-
-# Project Filtering
-project_label.pack()
-project_optmenu.pack()
-project_selectall_button.pack()
-project_checklist.pack(fill="y", expand=True)
-
-# Downloading
-download_button.pack()
-cur_download_progress.pack()
-cur_download_label.pack()
-all_download_progress.pack()
-all_download_label.pack()
+login_screen.pack()
 
 # APPLICATION EVENT LOOP
 root.mainloop()
