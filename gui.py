ffaa56edf0a40ac508152baf4685c6a0420c743a
silent-this-sonder <143755460+silent-this-sonder@users.noreply.github.com>
2026-03-26 16:16:45 -0400

    Add the rest of the missing widgets and write a new todo list

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 class ScrollableChecklist(tk.Frame):
     '''Create a list of checkbuttons that supports scrolling'''
@@ -41,13 +42,41 @@ login_button = tk.Button(root, text="Login")
 project_opts = ["all", "shared", "unshared"]
 project_label = tk.Label(root, text="Projects to Download")
 project_optmenu = tk.OptionMenu(root, tk.StringVar(value="all"), *project_opts)
+project_selectall_button = tk.Button(root, text="Select all")
 project_checklist = ScrollableChecklist(root, ["example"] * 50)
 
 # Downloading
-# TODO: download button
-# TODO: progress bar for current project
-# TODO: progress bar for all projects
-# TODO: labels for estimated time or numerical progress
+download_button = tk.Button(root, text="Download Selected")
+# progress bar for current project
+cur_download_progress = ttk.Progressbar(
+    root, orient="horizontal", length=500, mode="determinate"
+)
+# progress bar for all projects
+all_download_progress = ttk.Progressbar(
+    root, orient="horizontal", length=500, mode="determinate"
+)
+# labels for progress
+cur_download_label = tk.Label(
+    root,
+    text="Currently downloading [asset title], [num] / [total] assets downloaded"
+)
+all_download_label = tk.Label(
+    root,
+    text="Currently downloading [project title], [num] / [total] projects downloaded"
+)
+
+# FUNCTIONALITY
+
+# TODO: I want to organize the tkinter gui file a bit and make it only handle the widget display
+    # The code should be modular to keep it very neat
+    # I don't have the structure fully fleshed out yet but according to TkinterBuilder,
+    # they turn the gui file into a class so that the main file can just pass in data
+    # and receive output from the gui, which sounds pretty good.
+# TODO: make "Select all" actually select all
+# TODO: get project list everytime filter is reselected and show in the checklist
+# TODO: connect downloading to actual downloading code
+# TODO: code for progress bars to update based on info
+# TODO: code for progress labels to update based on info
 
 # GEOMETRY MANAGER
 
@@ -61,7 +90,15 @@ login_button.pack()
 # Project Filtering
 project_label.pack()
 project_optmenu.pack()
-project_checklist.pack(fill="both", expand=True)
+project_selectall_button.pack()
+project_checklist.pack(fill="y", expand=True)
+
+# Downloading
+download_button.pack()
+cur_download_progress.pack()
+cur_download_label.pack()
+all_download_progress.pack()
+all_download_label.pack()
 
 # APPLICATION EVENT LOOP
 root.mainloop()
