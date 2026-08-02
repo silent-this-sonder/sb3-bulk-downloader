72f0841c01994b1d8441a79a75b8ca06815358b0
silent-this-sonder <143755460+silent-this-sonder@users.noreply.github.com>
2026-08-02 14:09:08 +0000

    feat: fix typo in title label

--- gui.py
+++ gui.py
@@ -114,7 +114,7 @@ class ProjectSelectScreen(ft.View):
         self.vertical_alignment = "center"
 
         self.title = ft.Text(
-            "Project to Download",
+            "Projects to Download",
             size=32,
             weight="w600"
         )
