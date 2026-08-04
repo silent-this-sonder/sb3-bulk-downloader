e8aec933677009f13dd1464831ea8500d56a71bd
silent-this-sonder <143755460+silent-this-sonder@users.noreply.github.com>
2026-08-04 19:39:21 +0000

    fix: fix typo when trying to iterate over selected items in checklist

--- gui.py
+++ gui.py
@@ -194,7 +194,7 @@ class ProjectSelectScreen(ft.View):
     def get_selected_projects(self):
         '''Returns a list of the indices of checked boxes'''
         selected = []
-        for i in len(self.project_checklist.controls):
+        for i in range(len(self.project_checklist.controls)):
             checked = self.project_checklist.controls[i].value
             if checked:
                 selected.append(i)
